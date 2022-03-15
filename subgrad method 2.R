89452af1d49da3529f7df9bef613a5791040fc2d
Gabriel Riffo <52945978+Cab0512@users.noreply.github.com>
2022-03-15 15:39:05 -0300

    Update subgrad method 2.R

--- subgrad method 2.R
+++ subgrad method 2.R
@@ -1,11 +1,11 @@
 library(spatstat)
 library(cubature)
 N=200
-sig=0.5
+sig=1
 W=square(1)
 Warea=area(W)
 mu0=log(N/Warea)-sig/2
-phi=0.15
+phi=0.1
 X <- rLGCP(model="exp", mu=mu0, var=sig, scale=phi, win = W)
 Z<-attr(X,"Lambda")
 #covarianza=varianza^2*exp(-d*escala)
@@ -30,36 +30,39 @@ est<-function(par,par2,f,cova,j){
             flags=list(verbose=0, final=1),par=par,par2=par2,cova=cova,fgrad=f,j=j)
   a=f(P$d,par,par2,j)
   a=sum(a)-int$int
-  return(-a)
+  return(a)
 }
 #Waageralgo
 fwag<-function(d,par,par2,j){
-  return(cos(par2[j]*d))*(1*(d<=0.2) + 0* (d>=0.2))
+  return(exp(par[j])*exp(-1/exp(par2[j])*d))
+  #*(1*(d<=0.2) + 0* (d>=0.2))
 }
 cove<-function(d,par,par2){
   a=0
   for (i in 1:length(par)){
-    a=a+par[i]*cos(par2[i]*d)
+    a=a+exp(par[i])*exp(-1/exp(par2[i])*d)
     # falta la funcion k
   }
   return(a)
 }
-par=c(0.0005,0.0005,0.0005,0.0005,0.0005)
-par2=c(0.05,0.1,0.15,0.2,0.25)
-alp=10
-#k2=0.5
-lam=0.25
+par=c(log(0.5),log(0.5),log(0.5),log(0.5))
+par2=c(log(0.05),log(0.1),log(0.15),log(0.2))
+alp=0.001 #paso
+lam=0 #penalizacion
 est(para,par2,fwag,cove,1)
+int=vegas(inte, lowerLimit = rep(0, 4), upperLimit = rep(1, 4),
+          relTol=1e-3, absTol=1e-6,
+          flags=list(verbose=0, final=1),par=par,par2=par2,cova=cove,fgrad=fwag,j=1)
+a=fwag(P$d,par,par2,2)
+sum(a)
+int$int
 for (k in 1:100){
   para=par
-  print(par)
+  print(exp(par))
   for (i in 1:(length(par))){
-    gr=-est(para,par2,fwag,cove,i)
-    d=para[i]
-    if (abs(para[i]-gr)>lam){
-      d=-gr+sign(para[i]-gr)*lam
-    }
-    par[i]=para[i]+alp*d
+    x=para[i]-alp*est(para,par2,fwag,cove,i)
+    par[i]=sign(x)*pmax(abs(x)-lam*alp,0)
   }
 }
+
 

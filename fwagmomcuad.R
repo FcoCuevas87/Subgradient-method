5cbd858895ca3c0eace6edde5b42b4315746afbe
Gabriel Riffo <52945978+Cab0512@users.noreply.github.com>
2022-03-29 22:33:22 -0300

    Add files via upload

---
+++ fwagmomcuad.R
@@ -0,0 +1,149 @@
+library(spatstat)
+library(cubature)
+#simulacion LGCP
+N=500
+sig=0.1
+W=square(1)
+Warea=area(W)
+mu0=log(N/Warea)-sig/2
+phi=0.15
+X <- rLGCP(model="exp", mu=mu0, var=sig, scale=phi, win = W)
+
+Z<-attr(X,"Lambda")
+plot(Z)
+points(X,pch=16)
+X
+rmax=0.2
+P<-closepairs(X,0.2,twice = FALSE)
+
+#funciones necesarias
+
+#Funcion de covarianza
+cove<-function(d,par,par2){
+  a=0
+  for (i in 1:length(par)){
+    a=a+(par[i])^2*exp(-d/par2[i]) 
+    # falta la funcion k
+  }
+  return(a)
+}
+fwag<- function(d,par,par2,j){
+  return(2*par[j]*exp(-d/par2[j])*(1*(d<=0.2) + 0* (d>=0.2)))
+}
+fwag2<- function(d,par,par2,j){
+  return(2*exp(-d/par2[j])*(1*(d<=0.2) + 0* (d>=0.2)))
+}
+#integral1 
+integrand1<-function(arg,par,par2,j){
+  x1<-arg[1]
+  x2<-arg[2]
+  x3<-arg[3]
+  x4<-arg[4]
+  d=sqrt(((x1-x3)^2)+((x2-x4)^2))
+  w=exp(cove(d,par,par2))*fwag(d,par,par2,j)
+  return(w)
+}
+integrandM<-function(d,par,par2,j){
+  w=exp(cove(d,par,par2))*fwag2(d,par,par2,j)
+  return(w)
+}
+
+integrandj<-function(d,par,par2,i,j){
+  w=fwag(d,par,par2,i)*fwag(d,par,par2,j)*cove(d,par,par2)
+  return(w)
+}
+
+#gradiente
+gradien<-function(par,par2){
+  g=vector()
+  for (i in 1:length(par)){
+    g[i]=grad(par,par2,i)
+  }
+  return(g)
+}
+grad<-function(par,par2,j){
+  x0<-runifpoint(mc,win=square(1))
+  y0<-runifpoint(mc,win=square(1))
+  dU<-crosspairs(x0,y0,rmax,what="ijd")
+  inte=(1/mc)^2*sum(integrandM(dU$d,par,par2,j))
+  a=fwag(P$d,par,par2,j)
+  ver=sum(a)-(intensity(X))^2*inte
+  return(ver)
+}
+jacob<-function(par,par2){
+  x0<-runifpoint(mc,win=square(1))
+  y0<-runifpoint(mc,win=square(1))
+  dU<-crosspairs(x0,y0,rmax,what="ijd")
+  jac=matrix(nrow=length(par),ncol=length(par))
+  for (i in 1:length(par)){
+    for (j in 1:length(par) ){
+      temp=0
+      temp=-(intensity(X))^2*(1/mc)^2*sum(integrandj(dU$d,par,par2,i,j))
+      if(i==j){
+        temp=temp+sum(fwag2(P$d,par,par2,i))-(intensity(X))^2*(1/mc)^2*sum(integrandM(dU$d,par,par2,i))
+      }
+      jac[i,j]=temp
+    }
+  }}
+djacob<-function(par,par2){
+  x0<-runifpoint(mc,win=square(1))
+  y0<-runifpoint(mc,win=square(1))
+  dU<-crosspairs(x0,y0,rmax,what="ijd")
+  jac=c()
+  for (i in 1:length(par)){
+    temp=0
+    temp=-(intensity(X))^2*(1/mc)^2*sum(integrandj(dU$d,par,par2,i,i))
+    temp=temp+sum(fwag2(P$d,par,par2,i))-(intensity(X))^2*(1/mc)^2*sum(integrandM(dU$d,par,par2,i))
+    jac[i]=temp
+  }
+  return(jac)
+}
+
+mc=1000
+#varianzas (parametros a estimar)
+par=c(sqrt(0),sqrt(0.1))
+#phi
+par2=c(0.05,0.15)
+alp=0.000001
+lam=0
+print(jacob(par,par2))
+for (k in 1:200){
+  para=par
+  jac=djacob(par,par2)
+  jac=jac^{-1}
+  x0=para-(jac*gradien(par,par2))
+  par=x0
+  print(par^2)
+}
+for (k in 1:200){
+  print(par^2)
+  para=par
+  x0<-runifpoint(mc,win=square(1))
+  y0<-runifpoint(mc,win=square(1))
+  dU<-crosspairs(x0,y0,rmax,what="ijd")
+  jac=matrix(nrow=length(par),ncol=length(par))
+  for (i in 1:length(par)){
+    for (j in 1:length(par) ){
+      temp=0
+      temp=-(intensity(X))^2*(1/mc)^2*sum(integrandj(dU$d,par,par2,i,j))
+      if(i==j){
+        temp=temp+sum(fwag2(P$d,par,par2,i))-(intensity(X))^2*(1/mc)^2*sum(integrandM(dU$d,par,par2,i))
+      }
+      jac[i,j]=temp
+    }
+  }
+  #funcion jacob esta dando problemas
+  #jac=jacob(par,par2)
+  jac=solve(jac)
+  x0=para-(jac%*%gradien(par,par2))[,1]
+  par=x0
+}
+
+jac
+for (k in 1:200){
+  para=par
+  print(par)
+  for (i in 1:(length(par))){
+    x0=para[i]-alp*grad(par,par2,i)
+    par[i]=sign(x0)*pmax(abs(x0)-lam*alp,0)}
+}
